8d6d205d354370923519c59257d6f8f8750010f4
vipin <kumarvipinvk1997@gmail.com>
2020-10-16 20:01:11 +0530

    Updates

---
+++ precache-manifest.1c0ab1933cebecb5b42711b4aa84e300.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f2126276137f389db217a6c2481ee4",
+    "url": "/tic-tac-toe-2/index.html"
+  },
+  {
+    "revision": "e9c54bf971e9f530d601",
+    "url": "/tic-tac-toe-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63da910ceaf5f2084eea",
+    "url": "/tic-tac-toe-2/static/js/2.dd910ccf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tic-tac-toe-2/static/js/2.dd910ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c54bf971e9f530d601",
+    "url": "/tic-tac-toe-2/static/js/main.2e8340bc.chunk.js"
+  },
+  {
+    "revision": "b6ebda05a17e63c17a77",
+    "url": "/tic-tac-toe-2/static/js/runtime-main.2ff134ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
